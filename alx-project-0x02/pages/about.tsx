cfbe38ea524bc93243ce035ec5e990a57310cff8
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2025-07-27 14:22:23 +0100

    Feat: task 5 in progress

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from "@/components/common/Card";
+import PostModal from "@/components/common/PostModal";
+import { type PostData } from "@/interfaces";
 
 const about = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [post, setPost] = useState<PostData>({
+    userId: 1,
+    title: "",
+    content: "",
+  });
+
+  const handleAddPost = (newPost: PostData) => {
+    setPost({ ...newPost, id: Math.floor(Math.random() * 1000) + 1 });
+  };
   return (
     <div>
       <h1>about</h1>
+      <div>
+        <Card title={post?.title} content={post?.content} />
+        <button
+          onClick={() => setModalOpen(true)}
+          className="bg-blue-700 px-4 py-2 rounded-full text-white">
+          Add Post
+        </button>
+      </div>
+      {isModalOpen && (
+        <PostModal
+          onClose={() => setModalOpen(false)}
+          onSubmit={handleAddPost}
+        />
+      )}
     </div>
   );
 };
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,19 +1,8 @@
 import React, { useState } from "react";
 import Card from "@/components/common/Card";
-import PostModal from "@/components/common/PostModal";
-import { type CardProps, type PostData } from "@/interfaces";
+import { type CardProps } from "@/interfaces";
 
 const home = () => {
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [post, setPost] = useState<PostData>({
-    userId: 1,
-    title: "",
-    content: "",
-  });
-
-  const handleAddPost = (newPost: PostData) => {
-    setPost({ ...newPost, id: Math.floor(Math.random() * 1000) + 1 });
-  };
   const cardData: CardProps = {
     title: "Welcome to Daily Contents",
     content: "Here you can find the latest posts and user information.",
@@ -21,20 +10,7 @@ const home = () => {
   return (
     <div>
       <h1>home</h1>
-      <div>
-        <Card title={post?.title} content={post?.content} />
-        <button
-          onClick={() => setModalOpen(true)}
-          className="bg-blue-700 px-4 py-2 rounded-full text-white">
-          Add Post
-        </button>
-      </div>
-      {isModalOpen && (
-        <PostModal
-          onClose={() => setModalOpen(false)}
-          onSubmit={handleAddPost}
-        />
-      )}
+      <Card title={cardData.title} content={cardData.content} />
     </div>
   );
 };
