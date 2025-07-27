166903f8032e664360553c8d82e9d76e652e4de2
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2025-07-27 13:25:13 +0100

    Feat: task 3 update done

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Card from "@/components/common/Card";
+import { CardProps } from "@/interfaces";
 
 const home = () => {
+  const cardData: CardProps = {
+    title: "Welcome to Daily Contents",
+    content: "Here you can find the latest posts and user information.",
+  };
   return (
     <div>
       <h1>home</h1>
+      <Card title={cardData.title} content={cardData.content} />
     </div>
   );
 };
